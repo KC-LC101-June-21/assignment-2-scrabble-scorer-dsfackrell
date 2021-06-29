9969b1f7bef4c6c72973509c6e4ed82e3344d2c3
dsfackrell <84728935+dsfackrell@users.noreply.github.com>
2021-06-29 02:37:14 +0000

    Test

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -34,6 +34,7 @@ function oldScrabbleScorer(word) {
 
 function initialPrompt() {
    console.log("Let's play some scrabble! Enter a word:");
+   let wordToScore = input.question("Enter a word to score: ");
 };
 
 let simpleScore;
